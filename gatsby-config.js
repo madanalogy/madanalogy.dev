8529265da3e4fa7b6e2a18819a8d9d80682f5fa6
madanalogy <Ahmed_Bahajjaj@u.nus.edu>
2020-04-26 22:52:26 +0800

    Update Header.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    title: 'Ahmed Bahajjaj',
+    author: 'madanalogy',
+    description: 'Landing Page for Ahmed a.k.a. madanalogy',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,13 +8,13 @@ const Header = props => (
     </div>
     <div className="content">
       <div className="inner">
-        <h1>Dimension</h1>
+        <h1>Ahmed Bahajjaj</h1>
         <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
-          <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
+          Ex Digital Forensics Examiner, DevOps Engineer, and Web Developer.
+          <br/>
+          Undergraduate in <b>Computer Science</b> at the National University of Singapore.
+          <br/>
+          <b><u>**SITE IN DEVELOPMENT**</u></b>
         </p>
       </div>
     </div>
