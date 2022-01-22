75c8c48b2dfbd8efd75a02d53c68d4377cf4015c
Ahmed <42177597+madanalogy@users.noreply.github.com>
2022-01-23 01:05:50 +0800

    Update graphql

--- src/components/layout.js
+++ src/components/layout.js
@@ -31,7 +31,7 @@ const Layout = ({ children, location }) => {
         query SiteTitleQuery {
           site {
             siteMetadata {
-              title: Ahmed
+              title
             }
           }
         }
